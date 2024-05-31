b74153780c792420d88decbbd2ab8d6ec24ea8f7
Oleksandr Tkachuk <tksasha.me@gmail.com>
2024-05-31 12:46:39 +0300

    tiny refactored

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tksasha/hex
 
 go 1.22
+
+require go.mongodb.org/mongo-driver v1.15.0
--- main.go
+++ main.go
@@ -1,32 +1,13 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/hex"
 	"fmt"
-)
 
-const (
-	objectIDLength = 12
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func main() {
-	id, err := objectID()
-	if err != nil {
-		fmt.Printf("ERROR -> %v\n", err) //nolint:forbidigo
-
-		return
-	}
-
-	fmt.Println(id) //nolint:forbidigo
-}
-
-func objectID() (string, error) {
-	bytes := make([]byte, objectIDLength)
-
-	if _, err := rand.Read(bytes); err != nil {
-		return "", err
-	}
-
-	return hex.EncodeToString(bytes), nil
+	fmt.Println( //nolint:forbidigo
+		primitive.NewObjectID().Hex(),
+	)
 }
